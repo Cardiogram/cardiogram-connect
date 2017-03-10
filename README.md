62a6a7e31fe9ff33bf4a78aafaa4c7663224d9cb
Brandon Ballinger <brandon.ballinger+git@gmail.com>
2017-03-09 23:36:18 -0800

    README and example code to show OAuth2 Cardiogram Connect API.

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# CardiogramConnect
-API Documentation for Cardiogram Connect, shared with integration partners.
+# Cardiogram Connect
+
+API Documentation and example code for Cardiogram Connect. If you're here, you're likely an
+integration partner -- welcome! We're excited to work with you. Feel free to email
+connect@cardiogr.am with any questions.
+
+
+## Getting Started
+
+You should have received a client id and client secret from us, likely for both your development
+and production environments. Note that each client id and client secret will only work with a
+particular redirect URI on your system. If you need to support additional testing redirect URIs,
+please email connect@cardiogr.am.
+
+
+## User Authentication and Consent with OAuth 2
+
+Users explicitly choose to share data via a dialog that explains what they're sharing and
+with whom, similar to the Facebook Connect or "Sign in with Twitter" APIs. To obtain the user's consent,
+send them to the auth URL with your client id and redirect URI:
+
+  `https://cardiogr.am/auth?response_type=code&client_id=<YOUR_CLIENT_ID>&redirect_uri=<YOUR_REDIRECT_URI>/&scope=cardiograms`
+
+If the user clicks on "Yes, Share my data," you'll receive an temporary auth code as a parameter
+to your redirect URI:
+
+  `https://your.redirect.uri/goes/here?code=XXXXXXXXXX`
+
+You can then exchange that **auth code** for **access and refresh tokens** by posting to the
+/oauth/token URI. Note that your OAuth2 library may abstract the below token exchange process,
+but if you need to implement it explicitly, then make an HTTP Post similar to the below:
+
+```
+POST https://cardiogr.am/heart/oauth/token
+
+Headers
+  Authorization: 'Basic <Base64-encoded ClientId:ClientSecret>'
+  'Content-Type': 'application/x-www-form-urlencoded'
+
+Body:
+  grant_type: 'authorization_code',
+  client_id: <YOUR_CLIENT_ID>,
+  client_secret: <YOUR_CLIENT_SECRET>,
+  code: <AUTH_CODE>,
+```
+
+You'll receive a response with an access_token field, which you can use to access any of the
+following API endpoints on behalf of that particular user.
+
+
+# API Endpoints
+
+## Basic User Info: /oauth/basic_info
+Once you have an access token, you can get basic profile information, such as the user's timezone
+by issuing an HTTP get request to /oauth/basic_info:
+
+```
+GET https://cardiogr.am/oauth/basic_info
+
+Headers:
+  Authorization: Bearer <ACCESS_TOKEN>
+```
+
+Response:
+```
+  {
+  	native_user_id: 8100
+  }
+```
+
+
+## Metrics: /oauth/metrics
+Get the contents of the user's metrics pane--including resting heart rate, step counts, etc.--with
+the /oauth/metrics URI:
+
+```
+GET https://cardiogr.am/oauth/basic_info
+
+Headers:
+  Authorization: Bearer <ACCESS_TOKEN>
+```
+
+Response:
+```
+  {
+  	'2017-03-01': {RESTING_BPM: 76, STEPS: 9121, MOVE: 310, SLEEP_BPM: 52},
+  	'2017-03-02': {RESTING_BPM: 74, STEPS: 13733, MOVE: 491, SLEEP_BPM: 51},
+  	...
+  }
+```
+
+
+## Example Code
+
+You can see example code in simple-server.js. To run the server, run this command in your terminal:
+`CLIENT_SECRET=<YOUR_CLIENT_SECRET> PORT=9000 node simple-server.js`
---
+++ simple-server.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var path = require('path');
+var request = require('request').defaults({jar: true});
+
+function SimpleServer(clientSecret) {
+  this.baseUri = 'http://heartai-dev.herokuapp.com';
+  this.clientId = 'CardiogramConnectDemo_Local';  // must match index.html
+  this.clientSecret = clientSecret;
+};
+
+/**
+ * Starts a tiny server to handle the redirect URI, and once we receive the auth code, we POST it to
+ * /oauth/token to get access and refresh tokens.
+ */
+SimpleServer.prototype.startServer = function(port, cb) {
+  var self = this;
+  this.server = express();
+
+  this.server.use(express.static(path.join(__dirname, 'public/')))
+
+  // The initial HTML page that directs the user to https://cardiogr.am/auth?response_type=code&client_id=<YOUR_CLIENT_ID>&redirect_uri=<YOUR_REDIRECT_URI>/&scope=cardiograms
+  this.server.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+  // The below snippet of code shows you to exchange the auth code for an auth token.
+  this.server.get('/oauth/callback', function(req, res) {
+    var code = req.query.code;
+    if (!!req.query.error) {
+      res.json({message: 'Error in redirect: ' + req.query.error});
+    } else if (!code || typeof(code) !== 'string') {
+      res.json({message: 'Error, invalid auth code: '});
+    }
+    self.exchangeAuthCodeForTokens(code, (err, accessToken, refreshToken) => {
+      if (!!err) {
+        res.json({message: 'Error exchanging code for tokens', err: JSON.stringify(err)});
+      } else {
+        self.getMetrics(accessToken, (err, metrics) => {
+          if (!!err) {
+            res.json({message: 'Error getting metrics', err: err})
+          } else {
+            res.json({message: 'Success! Everything is working.', metrics: metrics});
+          }
+        });
+      }
+    });
+  });
+
+  this.server.listen(port, cb);
+};
+
+/**
+ * Exchanges the temporary auth code for access and refresh tokens.
+ */
+SimpleServer.prototype.exchangeAuthCodeForTokens = function(authCode, cb) {
+  var authorizationHeader = 'Basic ' + this._base64Encode(this.clientId + ':' + this.clientSecret);
+  request.post({
+    url: this.baseUri + '/heart/oauth/token',
+    headers: {
+      Authorization: authorizationHeader,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      grant_type: 'authorization_code',
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      code: authCode,
+    },
+  }, (err, httpResponse, body) => {
+    if (!!err) {
+      console.log(err);
+      return cb(err);
+    } else if (httpResponse.statusCode !== 200) {
+      console.log('non-200 status code: ' + httpResponse.statusCode);
+      return cb(new Error('HTTP code ' + httpResponse.statusCode + ': ' + JSON.stringify(body)));
+    } else {
+      tokens = JSON.parse(body)
+      return cb(null, tokens['access_token'], tokens['refresh_token']);
+    }
+  });
+};
+
+/**
+ * Get a JSON map of the user's metrics.
+ */
+SimpleServer.prototype.getMetrics = function(accessToken, cb) {
+  request.get({
+    url: this.baseUri + '/heart/oauth/metrics',
+    headers: {
+      Authorization: 'Authorization: Bearer ' + accessToken,
+    }
+  }, (err, httpResponse, body) => {
+    if (!!err) {
+      return cb(err);
+    } else if (httpResponse.statusCode !== 200) {
+      return cb(new Error('HTTP code ' + httpResponse.statusCode + ': ' + JSON.stringify(body)));
+    } else {
+      return cb(null, JSON.parse(body));
+    }
+  });
+};
+
+SimpleServer.prototype._base64Encode = function(string) {
+  return new Buffer(string).toString('base64');
+};
+
+// If this file is running as main, then start the server
+if (process.argv[1].indexOf('simple-server.js') > 0) {
+  if (!process.env.CLIENT_SECRET || !process.env.PORT) {
+    console.log('You must provide PORT and CLIENT_SECRET env variables.');
+    return;
+  }
+  new SimpleServer(process.env.CLIENT_SECRET).startServer(process.env.PORT, (err) => {
+    if (!!err) {
+      console.log(err);
+    } else {
+      console.log('Server is running at 127.0.0.1:' + process.env.PORT);
+    }
+  });
+}
